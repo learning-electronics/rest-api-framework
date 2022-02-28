44e096a397c713bf1e15ac171d1a8ce15d0b9ab2
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-02-28 15:59:36 +0000

    insignificant comments

--- account/api/views.py
+++ account/api/views.py
@@ -13,6 +13,7 @@ from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site  
 from django.utils.encoding import force_bytes, force_str 
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.shortcuts import redirect
 from .tokens import account_activation_token  
 from django.contrib.auth.models import User  
 from django.core.mail import EmailMessage 
@@ -52,10 +53,9 @@ def registration_view(request):
                 'domain': current_site.domain,  
                 'uid': urlsafe_base64_encode(force_bytes(account.pk)),  
                 'token': account_activation_token.make_token(account),  
-            })  
+            })
 
-            to_email = account.email #form.cleaned_data.get('email')  
-            print("aqui")
+            to_email = account.email
             email = EmailMessage(mail_subject, message, to=[to_email])  
             email.send()
         else:
@@ -75,6 +75,9 @@ def registration_view(request):
             data['m'] = ValidationError(str(e))
             return JsonResponse(data, safe=False)
 
+# View used for user activation
+# If sucessfull redirects to successeful page
+# If fails redirects to error page
 @csrf_exempt
 @api_view(["GET",])
 @permission_classes([AllowAny])
@@ -88,10 +91,10 @@ def activate(request, uidb64, token):
     if account is not None and account_activation_token.check_token(account, token):  
         account.is_active = True
         account.save()
-        return JsonResponse({ 'v': True, 'm': None }, safe=False)
+        return redirect('http://localhost:4200/success-register')
     else:  
         account.delete()
-        return JsonResponse({ 'v': False, 'm': 'Acctivation link is invalid!' }, safe=False) 
+        return redirect('http://localhost:4200/failed-register')
 
 # Everyone can acess this view
 # Receives JSON with keyswords "email", "password" that can't be null
