901e610ee109c00fbb3e0be4b46a179ea87dcccd
tiago <tamarques@ua.pt>
2022-03-21 03:15:19 +0000

    more views on classroom module and improvements

--- classroom/api/urls.py
+++ classroom/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from classroom.api.views import(
     add_classroom_view,
-    get_classes_view,
-    get_my_classes_view,
+    get_classrooms_view,
+    get_my_classrooms_view,
+    get_info_classroom_view,
     delete_classroom_view,
     enter_classroom_view,
 )
@@ -10,8 +11,9 @@ from classroom.api.views import(
 app_name = "classroom"
 
 urlpatterns = [
-    path("classrooms", get_classes_view, name="get_classrooms"),
-    path("my_classrooms", get_my_classes_view, name="get_my_classrooms"),
+    path("classrooms", get_classrooms_view, name="get_classrooms"),
+    path("my_classrooms", get_my_classrooms_view, name="get_my_classrooms"),
+    path("my_classrooms/<int:id>", get_info_classroom_view, name="get_my_classroom_info"),
     path("add_classroom", add_classroom_view, name="add_classroom"),
     path("delete_classroom/<int:id>", delete_classroom_view, name="delete_classroom"),
     path("enter_classroom/<int:id>", enter_classroom_view, name="enter_classroom"),
--- classroom/models.py
+++ classroom/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.hashers import make_password
 
 class Classroom(models.Model):
     id          = models.AutoField(primary_key=True, verbose_name="id")
-    name        = models.CharField(max_length=200, verbose_name="name")
+    name        = models.CharField(max_length=200, unique=True, verbose_name="name")
     teacher     = models.ForeignKey(Account, on_delete=models.CASCADE, limit_choices_to={'role': 2}, verbose_name="teacher", related_name='teacher')
     students    = models.ManyToManyField(Account, verbose_name="students", related_name='student', blank=True)
     date_created= models.DateField(verbose_name="date created", auto_now=True)
