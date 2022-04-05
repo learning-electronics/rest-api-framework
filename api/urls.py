5276074c42d581dc145e7febca2756431b924fa8
Hugo <h.domingos@ua.pt>
2022-04-05 15:21:46 +0100

    small fix on url

--- api/urls.py
+++ api/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('/admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     
     # Account URLS
     path("account/api/", include("account.api.urls", "account_api")),
