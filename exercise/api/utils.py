ac603b239ed20cad741adf4b863e85c5cdaa4141
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-04-05 15:03:26 +0100

    fixed changes

--- exercise/api/utils.py
+++ exercise/api/utils.py
@@ -1,2 +1,1 @@
-# Options of units available, add more if needed
-RULE_CHOICES = ( ("A", "Ampere"), ("V", "Volt"), ("W", "Watt"), ("Ohm", "Ohm") )
+RULE_CHOICES = ( ("A", "Ampere"), ("V", "Volt"), ("W", "Watt"), ("Ohm", "Ohm"), ("None", "Nenhuma") )
